be95688d65d74012d954bf0790b1258ae76d83d4
Himanshu Goyal <105408372+himanshuhoney30@users.noreply.github.com>
2024-07-02 12:59:58 +0530

    Update CheckButton.js

--- src/components/CheckButton.js
+++ src/components/CheckButton.js
@@ -15,7 +15,7 @@ const boxVariants = {
     background: 'var(--primaryPurple)',
     transition: { duration: 0.1 },
   },
-  unchecked: { background: 'var(--gray-2)', transition: { duration: 0.1 } },
+  unchecked: { background: 'var(--black-2)', transition: { duration: 0.1 } },
 };
 
 function CheckButton({ checked, handleCheck }) {
